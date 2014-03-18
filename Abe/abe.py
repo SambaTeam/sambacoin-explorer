e17a1e5105b211077a996c3ac0fea7564caeeadc
Danilo Lutz <danilolutz@users.noreply.github.com>
2014-03-18 00:39:23 -0300

    Update abe.py

--- Abe/abe.py
+++ Abe/abe.py
@@ -1192,7 +1192,7 @@ class Abe:
         return [
             '<form action="', page['dotdot'], 'search" style="margin: 50px 0"><div id="search" class="text-center">\n'
             '<input name="q" size="64" value="', escape(q), '" data-toggle="tooltip" data-placement="bottom" '
-            'title="" data-original-title="Search by address, block number or hash, transaction or public key hash,
+            'title="" data-original-title="Search by address, block number or hash, transaction or public key hash,'
             'or chain name. Address or hash search requires at least the first'
             HASH_PREFIX_MIN, ' characters."/>'
             '<button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-search"></span> Search</button>\n'
