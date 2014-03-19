e21f8aa1376927a39385c9b8b695f04b91f43426
Danilo Lutz <danilolutz@users.noreply.github.com>
2014-03-18 21:22:48 -0300

    Update abe.py

--- Abe/abe.py
+++ Abe/abe.py
@@ -64,7 +64,7 @@ DEFAULT_TEMPLATE = """
 <head>
     <link rel="shortcut icon" href="%(dotdot)s%(STATIC_PATH)sfavicon.ico" />
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
-    <link href="//netdna.bootstrapcdn.com/bootswatch/3.1.1/readable/bootstrap.min.css" rel="stylesheet">
+    <link href="//netdna.bootstrapcdn.com/bootswatch/3.1.1/flatly/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="%(dotdot)s%(STATIC_PATH)sabe.css" />
     
     <title>%(title)s</title>
